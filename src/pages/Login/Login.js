531f52e3e0968c803c0669b02a3523f61f2ed13e
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2022-12-28 22:48:46 +0600

    Login and signup done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,24 +1,94 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const Login = () => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { logIN, googleLogin } = useContext(AuthContext)
+    const [loginError, setLoginError] = useState('')
+
+    const [loginUserEmail, setLoginUserEmail] = useState('')
+    // const [token] = useToke(loginUserEmail)
+
+    const googleProvider = new GoogleAuthProvider();
+
+    let navigate = useNavigate();
+    let location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
+
+    // if (token) {
+    //     navigate(from, { replace: true });
+    // }
+
+    const handleLogin = data => {
+        console.log(data)
+        setLoginError(' ')
+        logIN(data.email, data.password)
+            .then((result) => {
+                const user = result.user;
+                console.log(user)
+                setLoginUserEmail(data.email)
+            })
+            .catch((error) => {
+                console.log(error.message)
+                setLoginError(error.message)
+            });
+    }
+
+
+
+    const handleGoogleLogin = () => {
+
+        // setLoader(true)
+
+        googleLogin()
+            .then((result) => {
+                const user = result.user;
+                setLoginUserEmail(user.email)
+                // setLoader(false);
+
+            }).catch((error) => {
+
+                const errorMessage = error.message;
+                toast.error(errorMessage)
+                // setLoader(false)
+
+
+            });
+
+
+    }
+
+
     return (
         <div className='flex justify-center items-center'>
             <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100">
                 <h1 className="text-2xl font-bold text-center">Login</h1>
-                <form novalidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+                <form onSubmit={handleSubmit(handleLogin)} novalidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+
                     <div className="space-y-1 text-sm">
-                        <label for="username" className="block dark:text-gray-400">Username</label>
-                        <input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md border dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        <label for="email" className="block dark:text-gray-400">Email</label>
+                        <input {...register("email", { required: "Email Address is required" })} type="text" name="email" id="email" placeholder="Email" className="w-full px-4 py-3 rounded-md border dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        {errors.email && <p className='text-red-500' role="alert">{errors.email?.message}</p>}
                     </div>
+
                     <div className="space-y-1 text-sm">
                         <label for="password" className="block dark:text-gray-400">Password</label>
-                        <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        <input {...register("password", { required: "Password Address is required", minLength: { value: 6, message: 'Passwor must be 6 characters or longer' } })} type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        {errors.password && <p className='text-red-500' role="alert">{errors.password?.message}</p>}
                         <div className="flex justify-end text-xs dark:text-gray-400">
                             <Link rel="noopener noreferrer" to="#">Forgot Password?</Link>
                         </div>
                     </div>
-                    <button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400 bg-yellow-500">Sign in</button>
+                    <button type="submit" className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400 bg-yellow-500">Sign in</button>
+                    {
+                        loginError && <p className='text-red-500'>{loginError}</p>
+                    }
                 </form>
                 <div className="flex items-center pt-4 space-x-1">
                     <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
@@ -26,7 +96,7 @@ const Login = () => {
                     <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
                 </div>
                 <div className="flex justify-center space-x-4">
-                    <button aria-label="Log in with Google" className="p-3 rounded-sm">
+                    <button onClick={handleGoogleLogin} aria-label="Log in with Google" className="p-3 rounded-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                             <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                         </svg>
